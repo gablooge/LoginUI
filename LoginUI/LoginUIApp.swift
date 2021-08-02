5a494607b945ba3a86a86c771119084954883542
Samsul Hadi <samsulhadikediri@gmail.com>
2021-08-02 09:34:39 +0700

    Initial Commit

---
+++ LoginUI/LoginUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginUIApp.swift
+//  LoginUI
+//
+//  Created by Samsul Hadi on 02/08/21.
+//
+
+import SwiftUI
+
+@main
+struct LoginUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
